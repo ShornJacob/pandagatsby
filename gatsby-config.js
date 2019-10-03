e45b4eec836ffac4025697f4a0f56acbbba5d8f5
Shorn Jacob <shorn@transparency.consulting>
2019-10-03 17:40:30 +1300

    Working Panda

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,15 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
